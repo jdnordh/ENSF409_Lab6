0f4ee14ba13ae331be0e0a9b9d50bf66851ca1db
PCJORDAN\Jordan <jdnordh@gmail.com>
2017-03-15 21:51:25 -0600

    Added exercise 3 to package, now it doesn't work

---
+++ src/exercise3/ReadRecord.java
@@ -0,0 +1,62 @@
+package exercise3;
+
+/** 
+ * Started by M. Moussavi
+ * March 2015
+ * Completed by: Jeremy Phillips and Jordan Nordh
+ */
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ReadRecord {
+    
+    private ObjectInputStream input;
+    
+    /**
+     *  opens an ObjectInputStream using a FileInputStream
+     */
+    private void readObjectsFromFile(String name)
+    {
+        MusicRecord record ;
+        
+        try
+        {
+            input = new ObjectInputStream(new FileInputStream(name));
+        } catch (IOException ioException) {
+            System.err.println( "Error opening file." );
+        }
+        
+        /* The following loop is supposed to use readObject method of of
+         * ObjectInputSteam to read a MusicRecord object from a binary file that
+         * contains several records.
+         * Loop should terminate when an EOFException is thrown.
+         */
+        try
+        {
+            while ( true )
+            {
+                record = (MusicRecord) input.readObject();
+                System.out.println(record.getYear() + " " + record.getSongName() + " " 
+                		+ record.getSingerName() + " " + record.getPurchasePrice());
+            }
+        } catch(EOFException e){
+            System.out.println( "EOF reached.");
+        } catch (ClassNotFoundException e) {
+            System.err.println( "Error: Class not found." );
+		} catch (IOException e) {
+            System.err.println( "Error: IO Exception." );
+		} catch (NullPointerException e){
+			System.err.println( "Error: Null pointer exception." );
+		}
+    }
+    
+    
+    public static void main(String [] args)
+    {
+        ReadRecord d = new ReadRecord();
+        d.readObjectsFromFile("allSongs.ser");
+    }
+}
